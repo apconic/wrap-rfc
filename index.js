81f7094b037ce03dd5580a1bc25fb25defa4f555
Suhail Ansari <suhaila@apconic.com>
2018-03-30 11:52:06 +0530

    Fixed some bugs related to promisify

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-'use strict';
+"use strict";
+const util = require("util");
+const { isEmpty } = require("lodash");
 
-var plattformPath = null;
-var util = require('util');
-var { isEmpty } = require('lodash');
+let plattformPath = null;
 
 if (process.platform === "linux" && process.arch === "x64") {
-  plattformPath = './build/linux_x64/rfc';
+  plattformPath = "./build/linux_x64/rfc";
 } else if (process.platform === "win32" && process.arch === "x64") {
-  plattformPath = './build/win32_x64/rfc';
+  plattformPath = "./build/win32_x64/rfc";
 } else {
-  console.log('Platform not supported', process.platform, process.arch);
+  console.log("Platform not supported", process.platform, process.arch);
 }
 
-var rfc = require(plattformPath);
+const rfc = require(plattformPath);
+const promisifiedConnect = util.promisify(rfc.Client.connect);
+const promisifiedInvoke = util.promisify(rfc.Client.invoke);
 
-var RFCClient = {
+const RFCClient = {
   client: null,
 
   async _getClient(connectionParams) {
@@ -22,10 +24,9 @@ var RFCClient = {
       try {
         this.client = new rfc.Client(connectionParams);
         if (isEmpty(this.client)) {
-          throw new Error('Unable to create client');
+          throw new Error("Unable to create client");
         }
-        var connectionPromise = util.promisify(this.client.connect);
-        await connectionPromise();
+        await promisifiedConnect();
         return this.client;
       } catch (connectionError) {
         console.log(`Error connecting to sap: ${connectionError.message}`);
@@ -40,12 +41,19 @@ var RFCClient = {
     try {
       var client = await this._getClient(connectionParams);
       try {
-        var invokePromise = util.promisify(client.invoke);
         console.log(`invoking ${functionModuleName}`);
-        var res = await invokePromise(client.invoke, functionModuleName, exportParam);
+        var res = await promisifiedInvoke(
+          client.invoke,
+          functionModuleName,
+          exportParam
+        );
         return res;
       } catch (invokeError) {
-        console.log(`Error invoking function module ${functionModuleName}: ${invokeError.message} `);
+        console.log(
+          `Error invoking function module ${functionModuleName}: ${
+            invokeError.message
+          } `
+        );
         console.log(JSON.stringify(invokeError));
         throw new Error(invokeError.message);
       }
@@ -54,7 +62,7 @@ var RFCClient = {
       console.log(JSON.stringify(connError));
       throw new Error(connError.message);
     }
-  },
+  }
 };
 
 module.exports = RFCClient;
